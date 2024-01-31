6851c48375a7369ea3081e4da0090cf37344c70d
DonovanMaillard <donovan.maillard@gmail.com>
2024-01-31 11:28:32 +0100

    add cor_taxref_gbif model
    add correspondance table between taxref cd_nom and gbif taxa_key

--- api2gn/models.py
+++ api2gn/models.py
@@ -12,3 +12,10 @@ class ParserModel(DB.Model):
     nb_row_last_import = DB.Column(DB.Integer)
     nb_row_last_import = DB.Column(DB.Integer)
     schedule_frequency = DB.Column(DB.Integer)
+
+
+class corTaxrefGbif(DB.Model):
+    __tablename__ = "cor_taxref_gbif"
+    __table_args__ = {"schema": "api2gn"}
+    cd_nom = DB.Column(DB.Integer, primary_key=True)
+    gbif_key = DB.Column(DB.Integer, nullable=False)
